4b240a951dc5038a4686888c9037cf3d46219416
jpoles1 <jpoles1@gmail.com>
2015-02-09 14:02:03 -0600

    Fixed issues with export of allele and genotype logs. Created functional plotting of changes in alleles and genotypes using ggplot.:

--- base.R
+++ base.R
@@ -1,3 +1,6 @@
+library(reshape2)
+library(ggplot2)
+library(gridExtra)
 genotypeFromAllele = function(p, q){
 	pp = p^2
 	pq = 2*p*q
@@ -27,19 +30,29 @@ evolution = function(gen, p, q, wpp, wpq, wqq){
 	while(i < gen){
 		newalleles = selection(p, q, wpp, wpq, wqq)
 		p=newalleles[1]; q=newalleles[2]
-		i=i+1;
 		allelehistory = rbind(allelehistory, cbind(i,p,q))
 		genohistory = rbind(genohistory, cbind(i, p^2, 2*p*q, q^2))
+		i=i+1;
 	}
-	c(allelehistory, genohistory)
+	colnames(allelehistory) = c("generation", "p", "q")
+	colnames(genohistory) = c("generation", "pp", "pq", "qq")
+	list(allelehistory, genohistory)
 }
 evolve = function(p = .5, gen = 50, wpp = 1, wpq = 1, wqq = 1){
 	q=1-p
 	x = evolution(gen, p, q, wpp, wpq, wqq)
-	alleles = x[1]
-	genotypes = x[2]
+	alleles = x[[1]]
+	genotypes = x[[2]]
 	#plot(alleles[,1], alleles[,2], col=2, ylim=c(0,1))
 	#points(alleles[,3], col=3)
-	plot(genotypes[,1], genotypes[,2], col=2, ylim=c(0,1))
-	points(genotypes[,3], col=3)
+	plot1 = ggplot(melt(alleles[,2:3]), aes(x=Var1, y=value, col=Var2))+geom_point()
+	plot2 = ggplot(melt(genotypes[,2:4]), aes(x=Var1, y=value, col=Var2))+geom_point()
+	grid.arrange(plot1, plot2, ncol=2, main=paste("Evolution with fitness",wpp))
+	#melt(alleles[,2:3])
+}
+fit = 1
+while(fit>0){
+	evolve(p=.5, gen = 50, wpp=fit)
+	fit = fit-.05
+	Sys.sleep(1.5)
 }
