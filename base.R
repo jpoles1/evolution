4aae3cd198bb79876fc21e52157fe15f5ffe0ec0
Jordan Poles <jpoles1@gmail.com>
2015-02-10 23:53:41 -0600

    Added mutation selection balance function.

--- base.R
+++ base.R
@@ -12,6 +12,12 @@ alleleFromGenotype = function(pp, pq, qq){
 	q = (qq) + (.5*pq);
 	c(p, q)
 }
+#Used to calculate the equilibriam frequency at which a given deleterious allele is created and destroyed at equal rates by mutation and selection respectively.
+#Selection coefficient is equal to 1-fitness
+#equilibrium frequency is equal to the sqrt of mutation rate to lethal allele / selection coefficient
+calculateMutationSelectionBalance = function(mutationRateLethal, fitness){
+  sqrt(mutation/(1-fitness))
+}
 selection = function(p, q, wpp, wpq, wqq){
 	x = genotypeFromAllele(p,q)
 	pp = x[1];pq = x[2];qq = x[3];
@@ -49,14 +55,12 @@ evolve = function(p = .5, gen = 50, wpp = 1, wpq = 1, wqq = 1, pmut=0, qmut=0){
 	x = evolution(gen, p, q, wpp, wpq, wqq, pmut, qmut)
 	alleles = x[[1]]
 	genotypes = x[[2]]
-	allelemax = round(max(alleles[50,2:3]), 2)
-	allelemin = round(min(alleles[50,2:3]), 2)
-	#plot(alleles[,1], alleles[,2], col=2, ylim=c(0,1))
-	#points(alleles[,3], col=3)
-	plot1 = ggplot(melt(alleles[,2:3]), aes(x=Var1, y=value, col=Var2))+ylim(0,1)+geom_point()+ggtitle(paste("Alleles Over Time; Final p=", round(alleles[gen,2],2), "q=", round(alleles[gen,3],2)))+xlab("Generation #")+ylab("Allele Freq.")+scale_color_discrete(name="Alleles", labels=c("p", "q"))
+	allelemax = round(max(alleles[gen,2:3]), 2)
+	allelemin = round(min(alleles[gen,2:3]), 2)
+  alleledat = melt(alleles[,2:3])
+  ggplot(alleledat, aes(x=alleledat$Var1, y=alleledat$value, col=alleledat$Var2))+ylim(0,1)+geom_point()+ggtitle(paste("Alleles Over Time; Final p=", round(alleles[gen,2],2), "q=", round(alleles[gen,3],2)))+xlab("Generation #")+ylab("Allele Freq.")+scale_color_discrete(name="Alleles", labels=c("p", "q"))
 	plot2 = ggplot(melt(genotypes[,2:4]), aes(x=Var1, y=value, col=Var2))+ylim(0,1)+geom_point()+ggtitle(paste("Genotypes Over Time; Final pp=", round(genotypes[gen,2],2), "pq=", round(genotypes[gen,3],2), "qq=",round(genotypes[gen,4],2)))+xlab("Generation #")+ylab("Genotype Freq.")+scale_color_discrete(name="Genotypes", labels=c("pp", "pq", "qq"))
 	grid.arrange(plot1, plot2, ncol=2, main=paste("Evolution with fitness: ","pp=",wpp,"; pq=",wpq,"; qq=",wqq, "\n and mutation rates p=", pmut, "; q=",qmut, sep=""))
-	#melt(alleles[,2:3])
 }
 changeFitness = function(){
 	fit = 1
